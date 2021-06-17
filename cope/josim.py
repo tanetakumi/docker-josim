95042351d06c77409ab230fa969ae5a8b3daa8d0
tanetakumi <nutanetakumi@yahoo.co.jp>
2021-06-17 05:22:03 +0000

    add judge.py and update structure

--- cope/josim.py
+++ cope/josim.py
@@ -4,29 +4,40 @@ import io
 import os
 import sys
 import matplotlib.pyplot as plt
+import judge
 
 def simulation(args) -> pd.DataFrame:
-    curdir = os.path.dirname(os.path.abspath(__file__))
-
+    # カレントディレクトリの取得
+    curdir = os.getcwd()
     if len(args) == 2:
-        print(curdir)
+        # filepath 
         filepath = curdir+"/"+args[1]
+        # if the file exists
         if os.path.exists(filepath):
-            # df = pd.read_csv(filepath,index_col=0)
+            # simulate josim and get the result 
+            # the result type is buffer
             result = subprocess.check_output( ["josim-cli", filepath,"-V", "1"] )
+            # decode the result and split result into progress and simulation result. 
             res_string = result.decode().split('100% Formatting Output')
+            # For reading dataframe, result replace ioString.
             return pd.read_csv(io.StringIO(res_string[1]),index_col=0,header=0)
+
+        # if the file doesn't exists
         else:
             print("ファイルが存在しません。\n指定されたパス:"+filepath)
+            sys.exit()
     else:
         print("引数が足りません。\n入力された引数:"+str(len(args)))
+        sys.exit()
 
 
 if __name__ == '__main__':
     df = simulation(sys.argv)
-    
+    dfj = judge.judge_frame(df)
     df.plot()
+    dfj.plot()
     plt.show()
+    
     
 
 
---
+++ cope/judge.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+def judge_frame(df : pd.DataFrame) -> pd.DataFrame:
+    df_diff = df.diff()
+    df_diff.where(df_diff >= 0, other = 0, inplace = True)
+
+    for indexs,items in df_diff.iteritems():
+        length = len(items)
+
+        # ---------------#
+        # 値の連続状態
+        # 開始index
+        # 合計
+        # ---------------#
+
+        keep = False
+        start_i = 0   
+        sum = 0        
+
+        for i in range(length):
+
+            if keep:
+                if items.iat[i] == 0:
+                    max_tmp = 0
+                    max_index = 0
+                    for j in range(start_i,i-1):
+                        if sum >3.14:
+
+                            
+                            if max_tmp < items.iat[j]:
+                                items.iat[max_index] = 0
+
+                                max_index = j
+                                max_tmp = items.iat[j]
+                                
+                                items.iat[j] = sum
+                                
+                            else:
+                                items.iat[j] = 0
+                        else:
+                            items.iat[j] = 0
+                        
+
+                    sum = 0
+                    keep = False
+                
+                else:
+                    sum += items.iat[i]
+            
+            else:
+                if items.iat[i] != 0:
+                    keep = True
+                    start_i = i
+                    sum += items.iat[i]
+    return df_diff
